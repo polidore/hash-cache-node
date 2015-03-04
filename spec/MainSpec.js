e87f85d2b25704cfceaab2197584135beac932ea
Ben Polidore <ben.polidore@itg.com>
2015-03-04 15:05:30 -0500

    more testing

--- spec/MainSpec.js
+++ spec/MainSpec.js
@@ -3,7 +3,9 @@ var hashcache = require('../index.js');
 describe("Hash Cash", function()  {
   it("Basic test of MD5", function() {
     var hashCache = hashcache('md5');
-    var hash = hashCache('MSFT-BUY');
-    expect(hash).toBe('io2Wq8klRaMIufOxNkX8SA');
+    var symbolSide = hashCache('MSFT-BUY');
+    var userSymbolSide = hashCache('9254-MSFT-BUY');
+    expect(symbolSide).toBe('io2Wq8klRaMIufOxNkX8SA');
+    expect(userSymbolSide).toBe('jAH1pT4TuUFVsl8KH-_IDQ');
   });
 });
